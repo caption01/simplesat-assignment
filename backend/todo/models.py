562ab44c1718f749b22dc0de12a326deb7c22fad
nutchapon hanouypornlert <salaryhero@nutchapons-MacBook-Air.local>
2023-04-06 11:49:06 +0700

    add migrate to remove unique from order field

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class TodoItem(models.Model):
     name = models.CharField(max_length=100)
-    order = models.IntegerField(null=False, unique=True)
+    order = models.IntegerField(null=False)
     is_done = models.BooleanField(default=False)
