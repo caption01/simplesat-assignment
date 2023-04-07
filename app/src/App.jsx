9f992f338c258a0693eac25c5edc46b81de880ba
nutchapon hanouypornlert <salaryhero@nutchapons-MacBook-Air.local>
2023-04-07 20:52:29 +0700

    install tailwind

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <h1 className="text-3xl font-bold underline-offset-1 text-orange-800">
+      Hello world!
+    </h1>
+  );
 }
 
 export default App;
